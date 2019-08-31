95d10b7ff97f25b5b4bbbe7fbf3e025a33e3169b
Jacek Kuczera <jacek.kuczera@gmail.com>
2019-09-01 00:36:42 +0200

    add new script for checking if there are new versions of modified packages

---
+++ check_for_new_versions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+get_version()
+{
+    pacman -Si $1 | grep ^Version | awk '{ print $NF }'
+}
+
+compare_versions()
+{
+    ver1=$(get_version "$1")
+    ver2=$(get_version "$2")
+    if [[ $ver1 != $ver2 ]] ; then
+        echo "$1 has different version than $2: $1 is $ver1, whereas $2 is $ver2"
+    fi
+}
+
+compare_versions "sxiv" "sxiv-large"
+compare_versions "networkmanager" "networkmanager_auto"
