07d3b243813cb2df7e4cb32ac48fe5a28b533d04
Jacek Kuczera <jacek.kuczera@gmail.com>
2020-08-12 21:11:43 +0200

    patch packages first

--- build_all.sh
+++ build_all.sh
@@ -13,13 +13,14 @@ do
     cp -a ${package} build/
 done < <(awk '/^[^#]/ {print $1}' ${SCRIPTDIR}/packages.txt)
 
-# Step 1: build patched packages
+# Step 1a: patch packages
+PATCHED_PACKAGES=$(awk '/^[^#]/ && NF==2' ${SCRIPTDIR}/packages.txt)
 while IFS="" read -r line || [ -n "$line" ]
 do
     package=${line%% *}
     vanilla=${line#* }
 
-    echo "Building patched package: ${package}..."
+    echo "Patching package: ${package}..."
     pushd .
 
     cd ${SCRIPTDIR}/build/${package} || { popd; echo "Cannot cd to package ${package}"; exit 1; }
@@ -28,21 +29,39 @@ do
     mv ${vanilla}/* .
     rm -rf ${vanilla} .git
     patch -F 3 -p0 < PKGBUILD.patch || { popd; echo "Cannot patch package ${package}"; exit 1; }
-    makepkg -cCsf --sign --noconfirm || { popd; echo "Cannot build package ${package}"; exit 1; }
 
     popd
-done < <(awk '/^[^#]/ && NF==2' ${SCRIPTDIR}/packages.txt)
+done <<< "$PATCHED_PACKAGES"
 
-# Step 2: build non-patched packages
+# Step 1b: build patched packages
 while IFS="" read -r line || [ -n "$line" ]
 do
     package=${line%% *}
 
-    echo "Building non-patched package: ${package}..."
+    echo "====================================================================="
+    echo "Building patched package: ${package}..."
+    echo "====================================================================="
     pushd .
 
     cd ${SCRIPTDIR}/build/${package} || { popd; echo "Cannot cd to package ${package}"; exit 1; }
     makepkg -cCsf --sign --noconfirm || { popd; echo "Cannot build package ${package}"; exit 1; }
 
     popd
-done < <(awk '/^[^#]/ && NF==1' ${SCRIPTDIR}/packages.txt)
+done <<< "$PATCHED_PACKAGES"
+
+# Step 2: build non-patched packages
+NON_PATCHED_PACKAGES=$(awk '/^[^#]/ && NF==1' ${SCRIPTDIR}/packages.txt)
+while IFS="" read -r line || [ -n "$line" ]
+do
+    package=${line%% *}
+
+    echo "====================================================================="
+    echo "Building non-patched package: ${package}..."
+    echo "====================================================================="
+    pushd .
+
+    cd ${SCRIPTDIR}/build/${package} || { popd; echo "Cannot cd to package ${package}"; exit 1; }
+    makepkg -cCsf --sign --noconfirm || { popd; echo "Cannot build package ${package}"; exit 1; }
+
+    popd
+done <<< "$NON_PATCHED_PACKAGES"
--- build_all_and_release.sh
+++ build_all_and_release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building packages..."
-./build_all.sh
+./build_all.sh || { exit 1; }
 
 echo "Releasing to github..."
-./release_to_github.sh
+./release_to_github.sh || { exit 1; }
